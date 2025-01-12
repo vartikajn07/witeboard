a965cefeff2126c887a107b04990bcdcf895b1eb
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-12 20:25:35 +0530

    getPos usage, Undo function

--- modules/room/helpers/Canvas.helpers.ts
+++ modules/room/helpers/Canvas.helpers.ts
@@ -13,9 +13,38 @@ export const drawFromSocket = (
     tempCtx.beginPath();
     socketMoves.forEach(([x, y]) => {
       tempCtx.lineTo(x, y);
-      tempCtx.stroke();
     });
+    tempCtx.stroke();
     tempCtx.closePath();
     afterDraw();
   }
+};
+
+//draw on undo function
+export const drawOnUndo = (
+  ctx: CanvasRenderingContext2D,
+  savedMoves: [number, number][][],
+  users: { [key: string]: [number, number][][] }
+) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  Object.values(users).forEach((user) => {
+    user.forEach((userMove) => {
+      ctx.beginPath();
+      userMove.forEach(([x, y]) => {
+        ctx.lineTo(x, y);
+      });
+      ctx.stroke();
+      ctx.closePath();
+    });
+  });
+
+  savedMoves.forEach((movesArr) => {
+    ctx.beginPath();
+    movesArr.forEach(([x, y]) => {
+      ctx.lineTo(x, y);
+    });
+    ctx.stroke();
+    ctx.closePath();
+  });
 };
