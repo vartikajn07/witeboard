5728721d88c299b25a5b59c298f4b28559443bfe
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-12 12:25:04 +0530

    state mgmt via recoil and socket actions

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,11 @@ import { createServer } from "http";
 
 import express from "express";
 import next, { NextApiHandler } from "next";
+import { Server } from "socket.io";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "@/common/types/global";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -11,6 +16,25 @@ const nextHandler = nextApp.getRequestHandler();
 nextApp.prepare().then(async () => {
   const app = express();
   const server = createServer(app);
+
+  const io = new Server<ClientToServerEvents, ServerToClientEvents>(server);
+
+  app.get("/health", async (_, res) => {
+    res.send("Healthy");
+  });
+
+  io.on("connection", (socket) => {
+    console.log("connection");
+
+    socket.on("draw", (moves, options) => {
+      console.log("drawing");
+      socket.broadcast.emit("socket_draw", moves, options);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`server ready & listening on ${port}`);
+    });
+  });
 
   app.all("*", (req: any, res: any) => {
     return nextHandler(req, res);
