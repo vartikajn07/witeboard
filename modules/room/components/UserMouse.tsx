d94906bbe916649fe029f48804a14da98658cf62
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-13 17:25:01 +0530

    user mouse component

--- modules/room/components/UserMouse.tsx
+++ modules/room/components/UserMouse.tsx
@@ -1,12 +1,15 @@
-//anonymous user cursor
+// user cursor / also anonymous user cursor
+//component
 
 import { useEffect, useState } from "react";
 import { useBoardPosition } from "../hooks/useBoardPosition";
 import { socket } from "@/common/lib/socket";
 import { motion } from "framer-motion";
 import { BsCursorFill } from "react-icons/bs";
+import { useRoom } from "@/common/recoil/room";
 
 export const UserMouse = ({ userId }: { userId: string }) => {
+  const { users } = useRoom();
   const boardPos = useBoardPosition();
   const [x, setX] = useState(boardPos.x.get());
   const [y, setY] = useState(boardPos.y.get());
@@ -39,10 +42,12 @@ export const UserMouse = ({ userId }: { userId: string }) => {
       className={`absolute top-0 left-0 text-blue-800 ${
         pos.x === -1 && "hidden"
       }pointer-events-none`}
+      style={{ color: users.get(userId)?.color }}
       animate={{ x: pos.x + x, y: pos.y + y }}
       transition={{ duration: 0.1, ease: "linear" }}
     >
       <BsCursorFill className="-rotate-90" />
+      <p className="ml-2">{users.get(userId)?.name || "Anonymous"}</p>
     </motion.div>
   );
 };
