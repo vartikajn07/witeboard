65f36a7d4a231d82070e7b2bec17b4dd412f559d
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-13 14:23:17 +0530

    new params in types

--- common/types/global.d.ts
+++ common/types/global.d.ts
@@ -10,33 +10,39 @@ interface Move {
   options: CtxOptions;
 }
 
-type Room = { users: Map<string, Move[]>; drawed: Move[] };
+type Room = {
+  usersMoves: Map<string, Move[]>;
+  drawed: Move[];
+  users: Map<string, string>;
+};
 
 interface ClientRoom {
   id: string;
-  // usersMoves: Map<string, Move[]>;
+  usersMoves: Map<string, Move[]>;
   movesWithoutUser: Move[];
   myMoves: Move[];
   users: Map<string, User>;
 }
 
 interface ServerToClientEvents {
-  room: (room: Room, usersToParse: string) => void;
+  room_exists: (exists: boolean) => void;
+  room: (room: Room, usersMovesToParse: string, usersToParse: string) => void;
   created: (roomId: string) => void;
   joined: (roomId: string, failed?: boolean) => void;
   user_draw: (move: Move, userId: string) => void;
   user_undo(userId: string): void;
   mouse_moved: (x: number, y: number, userId: string) => void;
-  new_user: (userId: string) => void;
+  new_user: (userId: string, username: string) => void;
   user_disconnected: (SocketId: string) => void;
 }
 
 interface ClientToServerEvents {
+  check_room: (roomId: string) => void;
   draw: (move: Move) => void;
   mouse_move: (x: number, y: number) => void;
   undo: () => void;
-  create_room: () => void;
-  join_room: (room: string) => void;
+  create_room: (username: string) => void;
+  join_room: (room: string, username: string) => void;
   joined_room: () => void;
   leave_room: () => void;
 }
