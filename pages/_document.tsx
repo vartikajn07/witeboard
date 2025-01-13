ed373cc0a6f6d498a1039685f7b1d522bafbb862
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-13 11:26:03 +0530

    imported modal onto app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        <div id="portal"></div>
         <Main />
         <NextScript />
       </body>
