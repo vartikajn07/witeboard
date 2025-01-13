ac643f4a7fdfc233fc72110df89c7b7f5555ad9f
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-13 13:32:05 +0530

    moved toolbar component to toolbar dir

---
+++ modules/room/components/toolbar/ToolBar.tsx
@@ -0,0 +1,33 @@
+import { useSetOptions } from "@/common/recoil/options";
+import ColorPicker from "./ColorPicker";
+import LineWidthPicker from "./LineWidthPicker";
+import { BsFillChatFill, BsFillImageFill, BsThreeDots } from "react-icons/bs";
+import { HiOutlineDownload } from "react-icons/hi";
+
+export const ToolBar = () => {
+  const setOptions = useSetOptions();
+
+  return (
+    <div
+      className="absolute left-10 top-[50%] z-50 flex flex-col gap-5 bg-black text-white"
+      style={{
+        transform: "translateY(-50%)",
+      }}
+    >
+      <ColorPicker />
+      <LineWidthPicker />
+      <button className="text-xl">
+        <BsFillChatFill />
+      </button>
+      <button className="text-xl">
+        <BsFillImageFill />
+      </button>
+      <button className="text-xl">
+        <BsThreeDots />
+      </button>
+      <button className="text-xl">
+        <HiOutlineDownload />
+      </button>
+    </div>
+  );
+};
