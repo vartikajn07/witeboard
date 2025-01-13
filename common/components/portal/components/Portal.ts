d83d4d3cd58f1dbecce2cc8a5c2a7a0960fa598b
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-13 11:13:29 +0530

    Portal component for rendering children

---
+++ common/components/portal/components/Portal.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import React, { JSX } from "react";
+
+import { createPortal } from "react-dom";
+
+const Portal = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
+  const [portal, setPortal] = useState<HTMLElement>();
+
+  useEffect(() => {
+    const node = document.getElementById("portal");
+    if (node) setPortal(node);
+  }, []);
+
+  if (!portal) return null;
+
+  return createPortal(children, portal);
+};
+
+export default Portal;
