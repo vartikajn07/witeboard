ec83267397670fa4048b92661b3a874aaebb3e7d
Vartika Jain <typicalvartika.jn@gmail.com>
2025-01-13 22:01:53 +0530

    modal box animation hook

--- common/recoil/options/options.atoms.ts
+++ common/recoil/options/options.atoms.ts
@@ -3,7 +3,11 @@ import { atom } from "recoil";
 export const optionsAtom = atom<CtxOptions>({
   key: "options",
   default: {
-    lineColor: "#0000",
+    lineColor: { r: 0, g: 0, b: 0, a: 1 },
+    fillColor: { r: 0, g: 0, b: 0, a: 0 },
     lineWidth: 5,
+    mode: "draw",
+    shape: "line",
+    selection: null,
   },
 });
